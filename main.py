bfce6fbab02b5aea82d63dca2b977974df773d4b
machoprogrammergg <codinglife35@gmail.com>
2025-04-03 20:34:29 +0200

    Updated debug mode

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = create_app()
 # If the script is run directly (not imported as a module)
 if __name__ == '__main__':
     # Run the Flask application with debug mode enabled and on port 8080
-    app.run(debug=True, port=8080)
+    app.run(debug=False)
