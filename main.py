a79ca7ecc4852f6748c97058360ee99e0524d727
machoprogrammergg <codinglife35@gmail.com>
2025-04-05 22:28:44 +0200

    Added app initiator function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import the create_app function from the website module
-from website import app
+from website import app, create_app
 from waitress import serve
 
 # Call the create_app function to initialize the Flask application
@@ -8,4 +8,5 @@ wsig_app = app
 # If the script is run directly (not imported as a module)
 if __name__ == '__main__':
     # Run the Flask application
+    create_app()
     serve(wsgi_app, host='0.0.0.0', url_prefix="/login")
