719609ade34e02692cd34bb7cfba5e791ca2499b
machoprogrammergg <codinglife35@gmail.com>
2025-04-07 00:16:36 +0200

    Minor min.py issue

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ from website import create_app
 # If the script is run directly (not imported as a module)
 if __name__ == '__main__':
     # Run the Flask application
-    create_app()
-    #serve(create_app(), host='0.0.0.0', port=8080)
+    serve(create_app(), host='0.0.0.0', port=8080)
