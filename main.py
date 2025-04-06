03e49bef83c4b4ebf0c0574f173b8d1751105f02
machoprogrammergg <codinglife35@gmail.com>
2025-04-06 22:59:35 +0200

    Added a venv

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from website import create_app
 
 # Call the create_app function to initialize the Flask application
 
-app = create_app()
 
 # If the script is run directly (not imported as a module)
 if __name__ == '__main__':
     # Run the Flask application
-    serve(app, host='0.0.0.0', port=8080)
+    serve(create_app(), host='0.0.0.0', port=8080)
